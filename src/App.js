cef8766d0a058c632f9a68e944a55ddca85619d8
GaganDeep1997 <gagan.deep@digitas.com>
2022-01-15 11:33:22 +0530

    adding bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+import Button from 'react-bootstrap/Button'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Fragment>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-6"><h1>Flikr Gallery</h1></div>
+                    <div className="col-6"><Button variant="primary">Primary</Button></div>
+                </div>
+            </div>
+        </Fragment>
+    );
 }
 
 export default App;
